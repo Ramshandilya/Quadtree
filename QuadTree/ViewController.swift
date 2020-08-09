b82c08715ada8116f5a9048583235c3bb9a0ff31
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2020-08-08 23:13:22 -0700

    Initial Commit

---
+++ QuadTree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuadTree
+//
+//  Created by Ramsundar Shandilya on 8/8/20.
+//  Copyright © 2020 Ramsundar Shandilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
